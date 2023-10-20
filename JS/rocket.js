f37f9afd574ba9ff5b1fe10578b18cd19af07b0c
lc1ml1 <144237409+lc1ml1@users.noreply.github.com>
2023-10-20 22:11:05 +0200

    all files submitted

---
+++ JS/rocket.js
@@ -0,0 +1,48 @@
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+	document.body.className = 'body-state'+state;
+	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
+
+	if (state == 2) {
+		timer = setInterval(function ()  {
+			countdownNumber = countdownNumber -1;
+			document.getElementById('countdown').innerHTML = countdownNumber;
+			// countdown 
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				// be nervous
+				document.getElementById('nervous').className = 'nervous show'; 
+			} else {
+				document.getElementById('nervous').className = 'nervous'; 
+			}
+
+			if (countdownNumber >1 && countdownNumber <= 4) {
+				// can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show'; 
+			} else {
+				document.getElementById('cant-wait').className = 'can-wait'; 
+			}
+
+			if (countdownNumber <= 0) {
+				changeState(3);
+			}
+		}, 600);
+	} else if (state == 3)  {
+		var success = setTimeout(function ()
+		{
+			var randomNumber = Math.round 
+				(Math.random()*10);
+
+			console.log('randomNumber: '+randomNumber)	
+
+			if (randomNumber > 4)  {
+				changeState(4);
+			} else {
+				changeState(5);
+			};
+		}, 2000);
+	};
+}
